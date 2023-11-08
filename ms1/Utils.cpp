e3f3ebe9d8c50a20b8c88a9842f3a55252bb7c8f
AndresSU23 <111714730+AndresSU23@users.noreply.github.com>
2023-11-07 23:22:03 -0500

    Update Utils.cpp
    Cstring include fix

--- ms1/Utils.cpp
+++ ms1/Utils.cpp
@@ -12,7 +12,8 @@
 ***********************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <ctime>
+#include <cstring>
+
 #include "Utils.h"
 using namespace std;
 namespace sdds {
